b6944194d46941233aea8a49364bc98db5d9c232
Boris Belyaev <bbor1974@gmail.com>
2025-02-11 14:49:24 +0400

    Initial commit

---
+++ cm.mk
@@ -0,0 +1,1 @@
+$(call inherit-product, device/explay/fog/cm_fog.mk)
---
+++ cm_fog.mk
@@ -0,0 +1,20 @@
+## Specify phone tech before including full_phone
+
+# Release name
+PRODUCT_RELEASE_NAME := Polaris
+
+# Inherit device configuration
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+# $(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
+$(call inherit-product, device/explay/fog/device.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 480
+TARGET_SCREEN_WIDTH := 800
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := fog
+PRODUCT_NAME := cm_fog
+PRODUCT_BRAND := Inet-Tablet
+PRODUCT_MODEL := N1 Plus
+PRODUCT_MANUFACTURER := Explay
