8ff368276cc49362b666c9836a3d9ad67790f0b8
Boris Belyaev <bbor1974@gmail.com>
2025-02-17 16:18:05 +0400

    BoardConfig: Add EGL stuff

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,6 +58,11 @@ TARGET_USERIMAGES_USE_EXT4 := true
 # EGL
 BOARD_EGL_CFG := $(LOCAL_PATH)/_prebuilt/system/lib/egl/egl.cfg
 USE_OPENGL_RENDERER := true
+BOARD_EGL_NEEDS_FNW := true
+TARGET_32_BIT_SURFACEFLINGER := true #use only 32bit lib
+
+# Workaround for no SYNC support
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
 
 # TWRP recovery
 TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
