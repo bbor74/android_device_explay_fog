1ad3f7f94e35cfb6db5f0099cc8a7da7c5781e92
Boris Belyaev <bbor1974@gmail.com>
2025-02-18 16:16:35 +0400

    Create a separate file for prebuilt blobs

--- cm_fog.mk
+++ cm_fog.mk
@@ -7,6 +7,7 @@ PRODUCT_RELEASE_NAME := Polaris
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 $(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
 $(call inherit-product, device/explay/fog/device.mk)
+$(call inherit-product, device/explay/fog/cm_fog-blobs.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 480
