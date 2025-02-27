0f73bda0506a242d80b1bc2dcbfdb857625ae4c6
Boris Belyaev <bbor1974@gmail.com>
2025-02-27 13:44:53 +0400

    Disable Bluetooth Manager Service

--- device.mk
+++ device.mk
@@ -14,6 +14,10 @@ PRODUCT_PACKAGES += \
 	audio.a2dp.default \
 	audio.r_submix.default
 
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+	config.disable_bluetooth=true
+
 # Ramdisk
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.sun8i:root/fstab.sun8i \
