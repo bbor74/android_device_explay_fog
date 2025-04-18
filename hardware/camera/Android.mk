5d069b22d0a15cf20537fa066c025fd36178e8a1
Boris Belyaev <bbor1974@gmail.com>
2025-04-18 10:26:35 +0400

    camera: Fix for Fog target device
    This fix color format in video capture

--- hardware/camera/Android.mk
+++ hardware/camera/Android.mk
@@ -59,11 +59,11 @@ ifneq ($(filter crane%,$(TARGET_DEVICE)),)
 LOCAL_CFLAGS += -D__SUN4I__
 endif
 
-ifneq ($(filter polaris%,$(TARGET_DEVICE)),)
+ifneq ($(filter fog,$(TARGET_DEVICE)),)
 LOCAL_CFLAGS += -D__SUN6I__
 endif
 
-ifneq ($(filter polaris%,$(TARGET_DEVICE)),)
+ifneq ($(filter fog,$(TARGET_DEVICE)),)
 LOCAL_CFLAGS += -D__SUNXI__
 endif
 
